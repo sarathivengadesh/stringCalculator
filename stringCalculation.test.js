75694798ff0376aa29872a8d406f20eab3e59d99
sarathivengadesh <sarathivengadesh97@gmail.com>
2024-07-15 16:22:35 +0530

    added gitignore File

---
+++ stringCalculation.test.js
@@ -0,0 +1,10 @@
+
+
+const {add} = require("./stringCalculation");
+
+
+
+// 1 Test add method as argumant as Empty string
+// 2 test Method as numer as string in a params
+// 3 test the multiple numbers as a string to pass as a params
+
---
+++ stringCalculation.ts
@@ -0,0 +1,6 @@
+
+  export const add=(value:string):number=>{
+  if(!value) return 0;
+  return 0
+}
+
