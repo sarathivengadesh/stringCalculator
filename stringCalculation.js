7e5d9736787a70a3a7fd44fd4bdc81a62a79406d
sarathivengadesh <sarathivengadesh97@gmail.com>
2024-07-16 07:48:50 +0530

    Throw error for if string has negative number

--- stringCalculation.js
+++ stringCalculation.js
@@ -1,9 +1,13 @@
 
 const add=(value)=>{
   if(!value || value.trim()==="") return 0;
-  const regex = /\d/g
-  const result = value?.match(regex)?.reduce((total,old)=>{return total+parseInt(old)},0);
-
+  const regex = /-?\d/g
+  const findNumbers = value?.match(regex)?.map(Number)|| [];
+  const findNegativeNum = findNumbers?.filter((n) => n < 0);
+ if(findNegativeNum?.length>0){
+   throw new Error (`Negative numbers not allowed: ${findNegativeNum?.join(',')}`);
+ }
+ const result = findNumbers?.reduce((total,old)=>{return total+old},0);
   return result;
 }
 
--- stringCalculation.test.js
+++ stringCalculation.test.js
@@ -4,6 +4,8 @@ const { add } = require('./stringCalculation');
 // 2 Pass number as string in params to get addtional number value
 // 3 Pass 2 number as string in params to get add value of all
 // 4 Pass multiple number as string in params to get add value of all
+// 5 Pass Mulit line number to the params
+// 6 Error case for Negative values
 
 describe('String Calculator', () => {
     test('Pass Empty string as params', () => { 
@@ -20,5 +22,9 @@ describe('String Calculator', () => {
      })
      test('handle new lines between numbers',()=>{
         expect(add("1\n2,3")).toBe(6);
+     })
+     test('Negative number Case',()=>{
+        expect(() => add("2,4,-2")).toThrow("Negative numbers not allowed: -2");
+        expect(() => add("2,-4,-1")).toThrow("Negative numbers not allowed: -4,-1");
      })
 });
